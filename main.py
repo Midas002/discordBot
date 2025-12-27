1c3c994c9f9f410648335ebe3bc1bc3e3e6f5d93
Midas002 <>
2025-12-27 17:14:35 +0100

    added some basic functionality

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+import discord
 
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f'Siema nie, tu {self.user}!')
+        
+    async def on_message(self, message):
+        if message.author==self.user:
+            return
+        
+        if message.content.startswith('hello'):
+            await message.channel.send(f'hello there {message.author}')
+        #print(f'Message from {message.author}: {message.content}')
+        
+ticket = open("BotTicket.txt",'r').read()        
+intents = discord.Intents.default()
+intents.message_content=True
 
-def main():
-    pass
-
-if __name__=='__main__':
-    main()
+client = Client(intents=intents)
+client.run(ticket)
